fec86cc4cb5026e2ec7c36c411212b364b6e3288
irene <keiko103@bu.edu>
2022-01-14 00:23:52 +0800

    [global] disable ssr

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  target: "serverless",
   reactStrictMode: true,
   images: {
     domains: ['cdn.fakercloud.com', 'via.placeholder.com'],
+  },
+  async rewrites() {
+    return [
+      // Rewrite everything to `pages/index`
+      {
+        source: "/:any*",
+        destination: "/",
+      },
+    ];
   },
 }
