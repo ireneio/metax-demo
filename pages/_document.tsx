788361a39b87c8f1f5f4a20227471e34ce608697
irene <keiko103@bu.edu>
2022-01-13 23:24:46 +0800

    clean up

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import createEmotionServer from '@emotion/server/create-instance';
-import createEmotionCache from '../createEmotionCache';
-import theme from '../modules/common/styles/theme';
+import * as React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import createEmotionServer from '@emotion/server/create-instance'
+import createEmotionCache from '../createEmotionCache'
+import theme from '../modules/common/styles/theme'
 
 export default class MyDocument extends Document {
   render() {
